3e5cfa7ab722ed281fe082ff440d941ea45b514d
= <alexandre.roussel.48@gmail.com>
2025-01-15 01:19:26 +0100

    feat: boilerplate for neural_network.py

--- dashboard/neural_network.py
+++ dashboard/neural_network.py
@@ -1,9 +1,6 @@
 import streamlit as st
+import os
+from PIL import Image
 
 def show_nn():
-    st.header("Neural Networks")
-    st.write("""
-    Neural Networks are computational models inspired by the human brain. They consist of layers of interconnected nodes (neurons) 
-    that process input data to make predictions. Deep learning, a subset of neural networks, involves large networks with 
-    many hidden layers to solve complex problems like image recognition, natural language processing, and more.
-    """)
+    st.header("Réseau de neurones")
